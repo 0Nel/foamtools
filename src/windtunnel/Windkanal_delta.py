5f28b3891361bb20d902b5b8d7a95cf98146b5a8
Aljoscha <aljoscha.sander@gmail.com>
2016-06-04 01:39:20 +0200

    update on the windtunnel tool

--- src/windtunnel/Windkanal_delta.py
+++ src/windtunnel/Windkanal_delta.py
@@ -4,97 +4,70 @@ import sys, getopt, os, numpy, glob, shutil
 import matplotlib.pyplot as plt
 
 # parameters for plotting
-params1 = {	'legend.fontsize' : 18,
-		'axes.titlesize' : 20, 	#Title 
-		'axes.labelsize' : 18	#Axenbeschriftung
-	  }
-plt.rcParams.update(params1)
+plotParameters = { 'legend.fontsize' : 18,
+					'axes.titlesize' : 20, 	#Title
+					'axes.labelsize' : 18	#Axenbeschriftung
+	  			 }
+plt.rcParams.update(plotParameters)
 
-# checks if the results directory already exists
-def checkdir():
-    print "checking directory"
+fluidParameters = 	{
+						'Ac' : 1,
+						'Uc' : 1,
+						'Rho' : 1
+					}
 
-# creates a new directory for the reynolds number results
-def mkdir(Re):
-    print "creating directory"
-# print the combined results into one file
-# REFACTOR
-def printRes(alpha, Cd, Cl, Cd_std, Cl_std, Re):		 #writes output data files
-    print "printing Results"
-    
+def parseLineToFloats(line):
+	try:
+		tmp = [float(string.replace(',', '.')) for string in line.split()]
+		if tmp:
+			return tmp
+	except:
+		return None
 
-# creates matplotlib plots with the combined data
-def plot_fig (alpha, Cd, Cl, Cd_std, Cl_std, Re):
-    print "plotting figures"
-    
-### reference function to convert mV/V to N
-# REFACTOR to generic regression analysis function
-# see commentary above
-def mVtoC(param, Cl, Ac, Uc):
-    print "mv to Cx"
-    
-# REFACTOR with get reference lift
-def getCalibration(files):
-    print "getting calibration references"
+def parseLineToDicts(line):
+	print "parsing line to dicts"
+	# try:
 
-def parseLine (line):
-    print "parsing lines"
 
-# refactor the parameters -> instead of allfiles the correct file path
-def average(allfiles, key, Re):                  #param = Dictionary and Key
-    print "averaging"
+# parses a file and return a 2D array
+# if the file is empty or no floats are found the function
+# will return None. The same happens if the file can't be opened
+def parseFile (filepath):
+	# try opening the input file
+	try:
+		f_in = open(filepath, 'r')
+	except(IOError):
+		print "could not open file:", filepath
+		return None
 
-def getKey(item):
-    print "getting key"
-    
-def merge(files, Re):
-    print "merging files"
-    
-def getData(filepaths):
-    print "collecting all input data"
+	tmp = []		# temporary array for parsing lines
+	tmp2D = []		# temporary array for building a 2D array
+	for line in f_in:
+		tmp = parseLineToFloats(line)
+		if tmp != None:
+			tmp2D.append(tmp)
+			tmp = []
 
-def getCalibration():
+	if tmp2D:
+		return numpy.array(tmp2D)
+	else:
+		return None
 
-    
+def parseConfigFile(filepath = '.windkanal.config'):
+	try:
+		f_in = open(filepath, 'r')
+	except(IOError):
+		print "could not open file:", filepath
+		return None
 
-    print "calculating calibration"
-    
-def getMount():
-    print "getting mount offset"
+	#for line in f_in:
 
-def getZero():
-    print "getting zero offset"
-
-def getDisk():
-    print "getting disk offset"
 
 #### START OF THE MAIN LOOP
 def main (argv):
+	file = raw_input("please provide a file: ")
+	print parseFile(file)
 
-    cal_files = True;
-    mount_files = False;
-    zero_files = False;
-    disk_files = False;
-
-    print "getting user input"
-
-    getData("/tmp/test")
-
-    if (cal_files):
-        getCalibration()
-
-    if (mount_files):
-        getMount()
-        
-    if (zero_files):
-        getZeor()
-    
-    if (disk_files):
-        getDisk()
-    
-    
-    
-    
 
 if __name__ == "__main__":
     main(sys.argv[1:])
